fe91aa0102947c31c39624f16071a0da856117ae
necchi486u16 <basbasuke0508@gmail.com>
2025-03-09 21:23:57 +0000

    Complete excluding Delete

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,9 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@rails/ujs";
+import "@hotwired/turbo-rails"
+
+import { Application } from "@hotwired/stimulus";
+import ConfirmationController from "./controllers/confirmation_controller";
+
+export const application = Application.start(document.documentElement);
+application.register("confirmation", ConfirmationController);
--- config/importmap.rb
+++ config/importmap.rb
@@ -2,3 +2,4 @@
 
 pin "application", preload: true
 pin "@rails/ujs", to: "rails-ujs.js"
+pin "@hotwired/turbo-rails", to: "turbo.min.js", preload: true
